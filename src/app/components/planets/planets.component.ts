49a0c61178392b29edfebc6cc2554f956f96343a
Mihai Puica <mihaipuica1@gmail.com>
2023-02-04 20:14:17 +0200

    added update for planet

--- src/app/components/planets/planets.component.ts
+++ src/app/components/planets/planets.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 import { Planet } from 'src/app/models/planet';
 import { PlanetService } from 'src/app/services/planet.service';
 
@@ -10,8 +11,19 @@ import { PlanetService } from 'src/app/services/planet.service';
 export class PlanetsComponent {
 
   planets: Planet[] = [];
+  selectedPlanet?: Planet;
 
-  constructor(private planetService: PlanetService) {}
+  planetForm = this.formBuilder.group({
+    id: 0,
+    name: '',
+    description: '',
+    status: '',
+    teamId: 0,
+    teamDto: null
+  });
+
+  constructor(private planetService: PlanetService,
+              private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
     this.getPlanets();
@@ -19,6 +31,18 @@ export class PlanetsComponent {
 
   getPlanets(): void {
     this.planetService.getPlanets()
-        .subscribe(planets => {this.planets = planets; console.log(planets)});
+        .subscribe(planets => this.planets = planets);
   }
+
+  onSelect(planet: Planet): void {
+    this.selectedPlanet = planet;
+    this.planetForm.controls['id'].setValue(planet.id);
+    this.planetForm.controls['name'].setValue(planet.name);
+    this.planetForm.controls['teamId'].setValue(planet.teamId);
+  }
+
+  onSubmit(): void {
+    this.planetService.updatePlanet(this.planetForm);
+  }
+
 }
--- src/app/services/planet.service.ts
+++ src/app/services/planet.service.ts
@@ -3,6 +3,7 @@ import { Observable } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { Planet } from '../models/planet';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Injectable({
   providedIn: 'root'
@@ -10,10 +11,32 @@ import { Planet } from '../models/planet';
 export class PlanetService {
 
   private URL = 'http://localhost:5186/api/Planet';
+  
 
   constructor(private http: HttpClient) { }
 
   getPlanets(): Observable<Planet[]> {
       return this.http.get<Planet[]>(this.URL+'/GetPlanets');
+  }
+
+  updatePlanet(planetForm: FormGroup): void {
+    var formData: any = new FormData();
+    formData.append('Id', planetForm.get('id')!.value);
+    formData.append('Name', planetForm.get('name')!.value);
+    formData.append('Description', planetForm.get('description')!.value);
+    formData.append('Status', planetForm.get('status')!.value);
+    formData.append('TeamId', planetForm.get('teamId')!.value);
+    formData.append('TeamDto', planetForm.get('teamDto')!.value);
+
+    const httpOptions = {
+      headers: new HttpHeaders({'Content-Type': 'application/json'})
+    }
+
+    this.http
+        .put(this.URL+'/UpdatePlanet', formData, httpOptions)
+        .subscribe({
+          next: (response) => console.log(response),
+          error: (error) => console.log(error)
+        })
   }
 }
